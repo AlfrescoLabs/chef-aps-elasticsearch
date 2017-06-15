33a0df633ea26a6245949aa84ed6f78cec412c7b
Abdul Mohammed <abdul.mohammed@alfresco.com>
2017-06-15 16:36:42 +0100

    Fix travis errors

--- test/integration/elasticsearch/inspec/plugin_spec.rb
+++ test/integration/elasticsearch/inspec/plugin_spec.rb
@@ -1,8 +1,8 @@
 # control 'Cloud-aws plugin' do
-  # impact 1.0
-  # title 'Check for cloud-aws plugin'
-  # desc 'Determine if cloud-aws plugin was installed successfully'
-  # describe command('/usr/local/elasticsearch/bin/plugin --list') do
-    # its(:stdout) { should match(/cloud-aws/) }
-  # end
+# impact 1.0
+# title 'Check for cloud-aws plugin'
+# desc 'Determine if cloud-aws plugin was installed successfully'
+# describe command('/usr/local/elasticsearch/bin/plugin --list') do
+# its(:stdout) { should match(/cloud-aws/) }
+# end
 # end
