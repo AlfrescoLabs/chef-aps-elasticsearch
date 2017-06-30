5adf047f2c7533822fe9967ef1fae6ae8ec652ab
Abdul Mohammed <abdul.mohammed@alfresco.com>
2017-06-30 14:21:37 +0100

    Make snapshot recipe private as it should only run in final chef-client run stage

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,4 +9,3 @@ include_recipe 'aps-elasticsearch::install-elasticsearch'
 include_recipe 'aps-elasticsearch::configure-elasticsearch'
 include_recipe 'aps-elasticsearch::plugin-install'
 include_recipe 'aps-elasticsearch::restart-elasticsearch'
-include_recipe 'aps-elasticsearch::create-snapshot-repo' if (node['aps-es']['ec2_discovery_enabled']) && (node['aps-es']['master_node'])
