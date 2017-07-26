08c2cc3f65ca6b0632a1bc78be584ab16edbe379
jferdousAl <juena.ferdous@alfresco.com>
2017-07-26 17:17:26 +0100

    inspec profile

--- test/integration/elasticsearch/inspec/es_version.rb
+++
@@ -1,8 +0,0 @@
-control 'ElasticSearch Version' do
-  impact 1.0
-  title 'Check for ElasticSearch Version'
-  desc 'Determine if ElasticSearch version is 1.7.3'
-  describe command("sleep 10 && curl -s -XGET 'localhost:9200' | python -c 'import json,sys; result=json.load(sys.stdin);print result[\"version\"][\"number\"]'") do
-    its(:stdout) { should match(/1.7.3/) }
-  end
-end
